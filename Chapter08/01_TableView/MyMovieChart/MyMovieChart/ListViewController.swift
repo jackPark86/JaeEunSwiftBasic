2fa4a0b2e0c01c48285aa80a3020a8fb158bd8f4
jackpark86 <jackpark086@gmail.com>
2021-06-28 13:02:10 +0900

    21.06.28 - 테이블 뷰와 데이터 소스 연동

--- Chapter08/01_TableView/MyMovieChart/MyMovieChart/ListViewController.swift
+++ Chapter08/01_TableView/MyMovieChart/MyMovieChart/ListViewController.swift
@@ -40,4 +40,27 @@ class ListViewController: UITableViewController {
     }//end of viewDidLoad
     
     
+    //테이블 뷰 행의 개수를 반환하는 메소드
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }//end of tableView()
+    
+    //테이블 뷰 행을 구성하는 메소드(개별 행을 만들어내는 역할)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = self.list[indexPath.row]
+        
+        //테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        cell.detailTextLabel?.text = row.description
+        return cell
+    }//end of tableView()
+    
+    //테이블 셀을 클릭하거 터치했을 때 액션 처리
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row)번째, 제목은 :  \(self.list[indexPath.row].title!)입니다!")
+    }//end of tableView
+    
+    
 }//end of class
